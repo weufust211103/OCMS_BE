19678a084b42ac60d61c01b25d13987810cb85da
trinh cuong <cuongvt203@gmail.com>
2025-03-26 10:46:07 +0700

    Add: More details to User Model

--- OCMS_WebAPI_Solution/OCMS_BOs/ViewModel/UserModel.cs
+++ OCMS_WebAPI_Solution/OCMS_BOs/ViewModel/UserModel.cs
@@ -12,5 +12,9 @@ namespace OCMS_BOs.ViewModel
         public string Username { get; set; }
         public string Email { get; set; }
         public string FullName { get; set; }
+        public string Gender { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
     }
 }
