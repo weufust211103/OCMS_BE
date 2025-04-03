523fa1d53b96d630506d7bba6f4c294f56da15d2
minhghhhh <mphamtran8@gmail.com>
2025-04-03 18:49:39 +0700

    data annotation

--- OCMS_WebAPI_Solution/OCMS_BOs/RequestModel/CourseDTO.cs
+++ OCMS_WebAPI_Solution/OCMS_BOs/RequestModel/CourseDTO.cs
@@ -1,6 +1,7 @@
 ﻿using OCMS_BOs.Entities;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,11 +17,22 @@ namespace OCMS_BOs.RequestModel
         public CourseStatus Status { get; set; }
         public Progress Progress { get; set; }
     }
+
     public class CreateCourseDTO
     {
+        [Required(ErrorMessage = "Course ID is required.")]
         public string CourseId { get; set; }
+
+        [Required(ErrorMessage = "Training Plan ID is required.")]
         public string TrainingPlanId { get; set; }
+
+        [Required(ErrorMessage = "Course Name is required.")]
+        [StringLength(100, ErrorMessage = "Course Name must not exceed 100 characters.")]
         public string CourseName { get; set; }
+
+        [Required(ErrorMessage = "Course Level is required.")]
+        [EnumDataType(typeof(CourseLevel), ErrorMessage = "Invalid Course Level.")]
         public CourseLevel CourseLevel { get; set; }
+
     }
 }
