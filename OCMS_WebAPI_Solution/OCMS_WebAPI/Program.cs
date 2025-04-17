5631189cc4b49ba126c47dec33c7c05ef4a0159c
trinh cuong <cuongvt203@gmail.com>
2025-04-17 23:32:08 +0700

    Feat: Add Log and add SignedBy to certificate

--- OCMS_WebAPI_Solution/OCMS_WebAPI/Program.cs
+++ OCMS_WebAPI_Solution/OCMS_WebAPI/Program.cs
@@ -14,6 +14,7 @@ using System.Text;
 using Azure.Identity;
 using Azure.Extensions.AspNetCore.Configuration.Secrets;
 using StackExchange.Redis;
+using OCMS_Services.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -209,5 +210,5 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
-
+app.UseApiAuditLogging(); 
 app.Run();
